5cb8bde28d4b95d74cf9500279d7dfcd8a413b8d
KOSASIH <kosasihg88@gmail.com>
2025-08-06 19:47:00 +0700

    Create About.tsx

---
+++ src/components/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About: React.FC = () => {
+    return (
+        <div className="about">
+            <h2>About Teos NFT Vault</h2>
+            <p>This project is dedicated to the cultural revival of Egypt through blockchain technology.</p>
+        </div>
+    );
+};
+
+export default About;
