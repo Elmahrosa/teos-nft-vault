719bb643466d98b62b9578b6040406bbe8cfdf75
KOSASIH <kosasihg88@gmail.com>
2025-08-06 19:50:25 +0700

    Create blockchain.ts

---
+++ src/services/blockchain.ts
@@ -0,0 +1,5 @@
+export const mintNFTOnBlockchain = async () => {
+    // Logic to interact with the blockchain and mint the NFT
+    console.log('Minting NFT on the blockchain...');
+    // Add your blockchain interaction code here
+};
