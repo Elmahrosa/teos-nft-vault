cd8d360d3730b739260adc246471e65a55f26908
KOSASIH <kosasihg88@gmail.com>
2025-08-06 19:47:37 +0700

    Create Vault.tsx

---
+++ src/components/Vault.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Vault: React.FC = () => {
+    return (
+        <div className="vault">
+            <h2>Your NFT Vault</h2>
+            <p>Manage your NFTs here.</p>
+        </div>
+    );
+};
+
+export default Vault;
